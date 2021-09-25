2e9872852d04e5d6675c93c7440bfc38a301f098
Easin Arafat <arafatmrx@gmail.com>
2021-09-25 09:04:20 +0600

    product style

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,24 @@
 import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
   console.log(props.product);
 
-  const { name, category } = props.product;
+  const { name, category, img, seller, price, stock } = props.product;
   return (
-    <div>
-      <h1>Hello Product is connected to shop</h1>
-      <h3>name: {name}</h3>
-      <h3>category: {category}</h3>
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <h2 className="product-name">{name}</h2>
+        <p>
+          <small>By : {seller}</small>
+        </p>
+        <p>stock:{stock}</p>
+        <h3>category: {category}</h3>
+        <h3>price: ${price}</h3>
+      </div>
     </div>
   );
 };
