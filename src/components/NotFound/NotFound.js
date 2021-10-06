0da67542cdd116348ad36cac7faf19a7379cfe2b
Easin Arafat <arafatmrx@gmail.com>
2021-10-06 22:00:11 +0600

    pages created

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 not found </h1>
+      <h3>don't ask me about this page</h3>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/components/OrderReview/OrderReview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OrderReview = () => {
+  return (
+    <div>
+      <h1>order review</h1>
+    </div>
+  );
+};
+
+export default OrderReview;
